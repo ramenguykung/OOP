b44a94bf21641be346feac9e3715397d6e903f13
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-11 15:42:56 +0700

    [ongoing] Lab content

--- src/GUI_L4.java
+++ src/GUI_L4.java
@@ -1,26 +1,44 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class GUI_L4 extends JFrame {
     JTextField jtfmonitor = new JTextField();
     JButton jbtmonitir = new JButton();
     GUI_L4() {
+        // attributes
+        JTextField jtfUSD = new JTextField(10);
+        JTextField jtfCAD = new JTextField(10);
+        JLabel jlbUSD = new JLabel("US Dollar");
+        JLabel jlbCAD = new JLabel("Canadian Dollar");
+
         JPanel p1 = new JPanel();
         p1.setLayout(new BorderLayout()); // Parent panel
         JPanel p2 = new JPanel();
-        p2.setLayout(new FlowLayout()); // Text field panel
+        p2.setLayout(new GridLayout(2, 1)); // Text field panel
         JPanel p3 = new JPanel();
-        p3.setLayout(new FlowLayout()); // Label panel
+        p3.setLayout(new GridLayout(2, 1)); // Label panel
         JPanel p4 = new JPanel();
         p4.setLayout(new BorderLayout()); // "Convert" Button panel
+
+        // p1 sub-panel
         p1.add(p2, BorderLayout.CENTER);
         p1.add(p3, BorderLayout.WEST);
         p1.add(p4, BorderLayout.SOUTH);
 
+        p2.add(jtfUSD);
+        p2.add(jtfCAD);
+        p3.add(jlbUSD);
+        p3.add(jlbCAD);
+        p4.add(new JButton("Convert"));
+
+        add(p1);
     }
     public static void main(String[] args) {
         JFrame frame = new GUI_L4();
+        frame.setTitle("Currency Converter");
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
