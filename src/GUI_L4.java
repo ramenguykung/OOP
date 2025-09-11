3ad9d3efe3978c2faced844c50c785891c19819a
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-11 08:07:24 +0000

    [ongoing] Lecture content

---
+++ src/GUI_L4.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GUI_L4 extends JFrame {
+    GUI_L4() {
+        
+    }
+    public static void main(String[] args) {
+        JFrame frame = new GUI_L4();
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
