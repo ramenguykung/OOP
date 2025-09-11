5cab5e9dcf7b537d99c09e17b8b9aa1fb1d67d62
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-11 06:42:39 +0000

    [ongoing] Lecture content

--- src/Jframe.java
+++ src/Jframe.java
@@ -2,6 +2,20 @@ import javax.swing.*;
 public class Jframe {
     public static void main(String[] args) {
         Jframe frame = new Ex();
+
+    }
+}
+class Ex extends JFrame{
+    JLabel jlbNamme = new JLabel("Name");
+    JLabel jlbSurname = new JLabel("Surname");
+    JLabel jlbAge = new JLabel("Age");
+    JTextField jtfName = new JTextField(10);
+    JTextField jtfSurname = new JTextField(10);
+    JTextField jtfAge = new JTextField(10);
+    JButton jbtnOK = new JButton("OK");
+    JButton jbtnCancel = new JButton("Cancel");
+
+    Ex() {
         
     }
 }
