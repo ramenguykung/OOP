87530f27ace4f90a218fda1a8fd8173bb173b70f
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-11 15:26:02 +0700

    [ongoing] Lab content

--- src/GUI_L4.java
+++ src/GUI_L4.java
@@ -1,8 +1,23 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.*;
 
 public class GUI_L4 extends JFrame {
+    JTextField jtfmonitor = new JTextField();
+    JButton jbtmonitir = new JButton();
     GUI_L4() {
-        
+        JPanel p1 = new JPanel();
+        p1.setLayout(new BorderLayout()); // Parent panel
+        JPanel p2 = new JPanel();
+        p2.setLayout(new FlowLayout()); // Text field panel
+        JPanel p3 = new JPanel();
+        p3.setLayout(new FlowLayout()); // Label panel
+        JPanel p4 = new JPanel();
+        p4.setLayout(new BorderLayout()); // "Convert" Button panel
+        p1.add(p2, BorderLayout.CENTER);
+        p1.add(p3, BorderLayout.WEST);
+        p1.add(p4, BorderLayout.SOUTH);
+
     }
     public static void main(String[] args) {
         JFrame frame = new GUI_L4();
