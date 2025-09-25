29d8fbba7aa27be3728a4a565efc9496ba547e16
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-25 15:23:20 +0700

    [ongoing] Lab content

--- src/Graphic_Demo1.java
+++ src/Graphic_Demo1.java
@@ -2,19 +2,27 @@ import java.awt.Graphics;
 import javax.swing.*;
 
 public class Graphic_Demo1 extends JFrame{
+    public Graphic_Demo1() {
+        setTitle("DrawArcs");
+        add(new ArcPanel());
+    }
     public static void main(String[] args) {
-    // TODO Continue from lecture sildes
+        Graphic_Demo1 frame = new Graphic_Demo1();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 300);
+        frame.setVisible(true);
     }
 }
 
-class DrawArc extends JPanel {
+class ArcPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         int xCenter = getWidth() / 2;
         int yCenter = getHeight() / 2;
-        int radius = (int) (Math.min(getWidth(), getHeight() * 2));
+        int radius = (int) (Math.min(getWidth(), getHeight()) * 0.4);
         
         int x = xCenter - radius;
         int y = yCenter - radius;
