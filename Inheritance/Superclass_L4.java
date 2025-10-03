2a3cf7ad9a2f4e6818673dd94c046f15b1011104
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-10-03 21:52:52 +0700

    Add Animal, Bird, and Dog classes with basic behaviors; implement GeometricObject hierarchy with Circle and Rectangle classes; introduce Superclass and Subclass demonstrating method overriding

---
+++ Inheritance/Superclass_L4.java
@@ -0,0 +1,22 @@
+public class Superclass_L4 {
+
+    private int num = 1;
+    protected int getNumber() {
+        return num;
+    }
+}
+
+class Subclass extends Superclass_L4 {
+
+    private int num = 10;
+    
+    @Override
+    protected int getNumber() {
+        return num + 5;
+    }
+
+    public static void main(String[] args) {
+        Subclass s = new Subclass();
+        System.out.println(s.getNumber());
+    }
+}
